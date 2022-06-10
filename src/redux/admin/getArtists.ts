3e2f7f688784d63b53b57cb542f5562c1adf4ec9
alex orlov <iobox420@gmail.com>
2022-06-10 14:57:13 +0500

    fix:auth v2

--- src/redux/admin/getArtists.ts
+++ src/redux/admin/getArtists.ts
@@ -10,13 +10,10 @@ export const getArtists = createAsyncThunk(
       const response = await AdminService.getArtists()
       console.log('Артисты успешно загружены')
       return fulfillWithValue(response.data)
-      /*return response.data*/
     } catch (e: any) {
       if(e.response.status === 401){
         dispatch(setAuth(false))
-/*        dispatch(setUser({} as IUser))*/
       }
-
       return rejectWithValue('Не удалось загрузить список артистов')
     }
   },
